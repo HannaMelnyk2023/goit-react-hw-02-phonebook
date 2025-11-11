b04f0affce209ecda810c3f25f328190e8489ed3
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-11-11 13:41:03 +0200

    structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from "react";
+import ContactForm from "./ContactForm/ContactForm";
+import ContactList from "./ContactList/ContactList";
+import { nanoid } from "nanoid";
+
+export default class App extends Component {
+  state = {
+  contacts: [],
+  name: ''
+  }
+  // оновлення name при введенні
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+  // новий контакт
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { name, contacts } = this.state;
+    
+}
+
+  }
+}
