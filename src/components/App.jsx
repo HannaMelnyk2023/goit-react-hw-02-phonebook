40fca10fc11ed08c4285816e98a61db36b0a5b77
HannaMelnyk2023 <151830548+HannaMelnyk2023@users.noreply.github.com>
2025-11-12 18:44:41 +0200

    step 3-finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,13 @@ import { nanoid } from 'nanoid';
 
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -32,8 +38,24 @@ export class App extends Component {
       number: '',//очистимо інпут
     });
   };
+  // оновлення фільтру
+  handleFilterChange = event => {
+    this.setState({
+      filter: event.target.value
+    })
+  };
+  // фільтр без регістру
+  getFilterContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contacts =>
+      contacts.name.toLowerCase().includes(normalizedFilter));
+  };
+
+
   render() {
-    const { contacts, name, number } = this.state;
+    const { name, number, filter } = this.state;
+    const filteredContacts = this.getFilterContacts();
     return (
       <div style={{ margin: '20px' }}>
         <h1>Phonebook</h1>
@@ -44,7 +66,15 @@ export class App extends Component {
           onSubmit={this.handleSubmit}
         />
         <h2>Contacts</h2>
-        <ContactList contacts={contacts} />
+        <label>Find contacts by name
+        <input
+          type="text"
+          name="filter"
+          value={filter}
+          onChange={this.handleFilterChange}
+        />
+        </label>
+        <ContactList contacts={filteredContacts} />
       </div>
     );
   }
